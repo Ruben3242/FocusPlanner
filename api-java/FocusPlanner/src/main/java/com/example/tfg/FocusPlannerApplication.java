639080f771058bed1adc13b64a05cd37a5e82c23
Ruben3242 <145350644+Ruben3242@users.noreply.github.com>
2024-11-09 13:20:58 +0100

    Spring Boot Structure

---
+++ api-java/FocusPlanner/src/main/java/com/example/tfg/FocusPlannerApplication.java
@@ -0,0 +1,13 @@
+package com.example.tfg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FocusPlannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FocusPlannerApplication.class, args);
+	}
+
+}
