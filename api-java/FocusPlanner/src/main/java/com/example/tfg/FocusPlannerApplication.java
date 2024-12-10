15f4f69f183594819d3b3df84c0d9b3ff99c54cc
Ruben3242 <145350644+Ruben3242@users.noreply.github.com>
2024-12-10 22:15:51 +0100

    creaccion de Perfiles
    creación de Perfiles en properties para cambiar de base de datos local a remota sencillamente

--- api-java/FocusPlanner/src/main/java/com/example/tfg/FocusPlannerApplication.java
+++ api-java/FocusPlanner/src/main/java/com/example/tfg/FocusPlannerApplication.java
@@ -1,11 +1,22 @@
 package com.example.tfg;
 
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FocusPlannerApplication {
+
+	@Value("${spring.profiles.active}")
+	private String activeProfile;
+
 	public static void main(String[] args) {
 		SpringApplication.run(FocusPlannerApplication.class, args);
+	}
+
+	@PostConstruct
+	public void logActiveProfile() {
+		System.out.println("***Active Spring Profile: " + activeProfile + " ***");
 	}
 }
