c178d7eb6a63a8079e86c09c874f070de49e71ac
ICE-7\rloru <ruben-lopez1@eep-igroup.com>
2025-01-20 11:40:44 +0100

    aplicando base de datos online railway

--- api-java/FocusPlanner/src/main/java/com/example/tfg/model/User.java
+++ api-java/FocusPlanner/src/main/java/com/example/tfg/model/User.java
@@ -4,7 +4,7 @@ import jakarta.persistence.*;
 import java.util.List;
 
 @Entity
-@Table(name = "user")
+@Table(name = "app_user")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
